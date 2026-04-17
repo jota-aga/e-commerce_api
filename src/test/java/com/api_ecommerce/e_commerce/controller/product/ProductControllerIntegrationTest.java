768d62cb13ae206a7111dbf53a5c714cacf0b5cf
joão henrique <joaolgg2estudos@gmail.com>
2026-04-17 19:38:31 -0300

    termino dos testes unitários da classe ProductControllerTest e começo dos testes de integração

---
+++ src/test/java/com/api_ecommerce/e_commerce/controller/product/ProductControllerIntegrationTest.java
@@ -0,0 +1,93 @@
+package com.api_ecommerce.e_commerce.controller.product;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.api_ecommerce.e_commerce.creator.ProductCreator;
+import com.api_ecommerce.e_commerce.dto.product.ProductRequest;
+import com.api_ecommerce.e_commerce.entity.Category;
+import com.api_ecommerce.e_commerce.entity.Product;
+import com.api_ecommerce.e_commerce.enums.ProductStatus;
+import com.api_ecommerce.e_commerce.repository.CategoryRepository;
+import com.api_ecommerce.e_commerce.repository.ProductRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+public class ProductControllerIntegrationTest {
+	
+	private static String BASE_URL = "/product";
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Autowired
+	private ObjectMapper objectMapper;
+	
+	@Autowired
+	private ProductRepository productRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	private Category category;
+	
+	private ProductRequest productRequest;
+	
+	@BeforeEach
+	public void setUp() {
+		category = Category.builder()
+									.name("Category")
+									.build();
+		category = categoryRepository.save(category);
+		
+		productRequest = new ProductRequest("name", "description", new BigDecimal(100), category.getId(), 100, ProductStatus.UNAVAILABLE);
+	}
+	
+	@Test
+	public void createProduct_Sucess() throws Exception {
+		mockMvc.perform(post(BASE_URL)
+				.with(jwt().authorities(new SimpleGrantedAuthority("SCOPE_ADMIN")))
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(productRequest)))
+				.andExpect(status().isCreated());
+		
+		Optional<Product> optionalProduct = productRepository.findByName(productRequest.name());
+		
+		assertTrue(optionalProduct.isPresent());
+	}
+	
+	@Test
+	public void whenNameIsRepeated() throws Exception {
+		mockMvc.perform(post(BASE_URL)
+				.with(jwt().authorities(new SimpleGrantedAuthority("SCOPE_ADMIN")))
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(productRequest)));
+		
+		mockMvc.perform(post(BASE_URL)
+				.with(jwt().authorities(new SimpleGrantedAuthority("SCOPE_ADMIN")))
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(productRequest)))
+				.andExpect(status().isConflict());
+	}
+	
+	
+}
--- src/test/java/com/api_ecommerce/e_commerce/controller/product/ProductControllerTest.java
+++ src/test/java/com/api_ecommerce/e_commerce/controller/product/ProductControllerTest.java
@@ -1,11 +1,17 @@
 package com.api_ecommerce.e_commerce.controller.product;
 
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.verify;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.util.Collection;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -22,6 +28,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import com.api_ecommerce.e_commerce.controller.ProductController;
 import com.api_ecommerce.e_commerce.creator.ProductCreator;
 import com.api_ecommerce.e_commerce.dto.product.ProductRequest;
+import com.api_ecommerce.e_commerce.entity.Product;
+import com.api_ecommerce.e_commerce.exceptions.ConflictException;
+import com.api_ecommerce.e_commerce.exceptions.NotFoundException;
 import com.api_ecommerce.e_commerce.infra.SecurityConfiguration;
 import com.api_ecommerce.e_commerce.service.ProductService;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -42,10 +51,15 @@ public class ProductControllerTest {
 	
 	private ProductRequest productRequest;
 	
+	private Product productAvailable;
+	
+	private Product productUnavailable;
+	
 	@BeforeEach
 	public void setUp() {
 		productRequest = ProductCreator.availableProductRequest();
-		
+		productAvailable = ProductCreator.productAvaliable();
+		productUnavailable = ProductCreator.productUnavaliable();
 	}
 	
 	@Test
@@ -57,5 +71,59 @@ public class ProductControllerTest {
 				.andExpect(status().isCreated());
 		
 		verify(productService).createProduct(productRequest);
+	}
+	
+	@Test
+	public void createProduct_whenNameIsRepeated() throws Exception {
+		doThrow(new ConflictException("message")).when(productService).createProduct(productRequest);
+		
+		mockMvc.perform(post("/product")
+				.with(jwt().authorities(new SimpleGrantedAuthority("SCOPE_ADMIN")))
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(productRequest)))
+				.andExpect(status().isConflict());
+	}
+	
+	@Test
+	public void findProductsByName() throws Exception {
+		String paramNameValue = productAvailable.getName();
+		when(productService.findAllByName(paramNameValue)).thenReturn(List.of(productAvailable, productUnavailable));
+		
+		mockMvc.perform(get("/product/name")
+				.param("name", paramNameValue))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$", hasSize(2)));
+	}
+	
+	@Test
+	public void findProductsByCategory() throws Exception {
+		String paramCategoryValue = productAvailable.getCategory().getName();
+		when(productService.findAllByCategory(paramCategoryValue)).thenReturn(List.of(productAvailable, productUnavailable));
+		
+		mockMvc.perform(get("/product/category")
+				.param("categoryName", paramCategoryValue))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$", hasSize(2)));
+	}
+	
+	@Test
+	public void updateProduct_Sucess() throws Exception {
+		
+		mockMvc.perform(put("/product/"+productAvailable.getId())
+				.with(jwt().authorities(new SimpleGrantedAuthority("SCOPE_ADMIN")))
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(productRequest)))
+				.andExpect(status().isOk());
+	}
+	
+	@Test
+	public void updateProduct_WhenProductOrCategoryNotFound() throws Exception {
+		doThrow(new NotFoundException("message")).when(productService).updateProduct(productAvailable.getId(), productRequest);
+		
+		mockMvc.perform(put("/product/"+productAvailable.getId())
+				.with(jwt().authorities(new SimpleGrantedAuthority("SCOPE_ADMIN")))
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(productRequest)))
+				.andExpect(status().isNotFound());
 	}
 }

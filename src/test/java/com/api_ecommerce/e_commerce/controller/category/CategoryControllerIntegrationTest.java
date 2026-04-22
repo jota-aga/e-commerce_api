e4df32a93c726b1319f19e81ccba32cdd088c640
joão henrique <joaolgg2estudos@gmail.com>
2026-04-22 00:01:27 -0300

    removendo importações inúteis e criando novo método de criação de category'

--- src/test/java/com/api_ecommerce/e_commerce/controller/category/CategoryControllerIntegrationTest.java
+++ src/test/java/com/api_ecommerce/e_commerce/controller/category/CategoryControllerIntegrationTest.java
@@ -1,19 +1,44 @@
 package com.api_ecommerce.e_commerce.controller.category;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.api_ecommerce.e_commerce.dto.category.CategoryDTO;
+import com.api_ecommerce.e_commerce.entity.Category;
+import com.api_ecommerce.e_commerce.repository.CategoryRepository;
 import com.api_ecommerce.e_commerce.repository.ProductRepository;
 import com.api_ecommerce.e_commerce.service.CategoryService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
 public class CategoryControllerIntegrationTest {
+	
+	public static String BASE_URL = "/category";
+	
+	public static String ROLE = "SCOPE_ADMIN";
 	
 	@Autowired
 	private MockMvc mockMvc;
@@ -26,4 +51,27 @@ public class CategoryControllerIntegrationTest {
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	private CategoryDTO dto;
+	
+	@BeforeEach
+	public void setUp() {
+		dto = new CategoryDTO("category");
+	}
+	
+	@Test
+	public void saveCategory_Sucess() throws JsonProcessingException, Exception {
+		mockMvc.perform(post(BASE_URL)
+				.with(jwt().authorities(new SimpleGrantedAuthority(ROLE)))
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(dto)))
+				.andExpect(status().isCreated());
+				
+		Optional<Category> optionalCategory = categoryRepository.findByName(dto.name());
+		
+		assertTrue(optionalCategory.isPresent());
+	}
 }
--- src/test/java/com/api_ecommerce/e_commerce/controller/category/CategoryControllerTest.java
+++ src/test/java/com/api_ecommerce/e_commerce/controller/category/CategoryControllerTest.java
@@ -1,16 +1,13 @@
 package com.api_ecommerce.e_commerce.controller.category;
 
-import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.BeforeEach;
